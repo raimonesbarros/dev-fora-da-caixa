19028a283a789eae1e15beec6143f35f956eab99
RAIMONES BARROS <raimonesrg3@gmail.com>
2023-01-10 20:10:09 -0300

    json criado

--- js/script.js
+++ js/script.js
@@ -1,21 +1,16 @@
-"use strict"
-//PEGAR INFO DE LOGIN
-const usuario=document.querySelector("#email-login")
-const senha=document.querySelector("#password-login")
+// Menu Sandwich
+const sandwich = document.querySelector(".full-sandwich");
+const s1       = document.querySelector(".s1");
+const s2       = document.querySelector(".s2");
+const s3       = document.querySelector(".s3");
+const navMob   = document.querySelector(".nav-mobile");
 
-// PEGAR INFO DE CADASTRO
-
-// PEGAR INFO DO SANDWICH
-const sandwich=document.querySelector(".full-sandwich")
-const s1=document.querySelector(".s1")
-const s2=document.querySelector(".s2")
-const s3=document.querySelector(".s3")
-const navMob=document.querySelector(".nav-mobile")
-
+// Chamar animacoes css
 sandwich.addEventListener("click",()=>{
-    s1.classList.toggle("enter-s1")
-    s2.classList.toggle("enter-s2")
-    s3.classList.toggle("enter-s3")
-    sandwich.classList.toggle("move")
-    navMob.classList.toggle("enter-nav")
+    s1.classList.toggle("enter-s1");
+    s2.classList.toggle("enter-s2");
+    s3.classList.toggle("enter-s3");
+    sandwich.classList.toggle("move");
+    navMob.classList.toggle("enter-nav");
 })
+//fim menu sandwich
