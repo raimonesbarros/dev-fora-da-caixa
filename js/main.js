6152c70dccc21fe4008c3592e16cba4867647aa1
RAIMONES BARROS <raimonesrg3@gmail.com>
2023-01-15 13:24:49 -0300

    msg dinâmica

--- js/main.js
+++ js/main.js
@@ -2,41 +2,51 @@ import { UsuarioController } from '../js/poo/controller/UsuarioController.js'
 
 // Formulário de Cadastro
 const inputs       = [...document.querySelectorAll("input:not([type=radio])")]
-const sexo         = [...document.querySelectorAll("[type=radio]")];
+const nascimmento  = document.querySelector("input[type=date]")
+const sexo         = [...document.querySelectorAll("[type=radio]")]
 const labelsexo    = document.querySelector(".labelsexo")
 const senha        = document.querySelector("#password-register")
 const repetirSenha = document.querySelector("#repeat-password")
+const messenger    = document.querySelector(".messenger")
 
 // Validação
 let verify1 = true;
 // Expressão de erro
-function erro(){
-    labelsexo.classList.add("vazio")
+function erro(local){
+    local.classList.add("vazio")
     verify1=false
 }
 //Verificando condições exigidas
 function verify(){
+    // senhas diferentes ou menores que 8 digitos
+    if(senha.value!==repetirSenha.value){
+        erro(repetirSenha)
+        msgAlert("Senhas NÃO conferem!", 'alert')
+    } else if(senha.value.length < 8){
+        erro(senha)
+        msgAlert("Senha muito curta!", 'alert')
+    }
     // buscar radio marcado
     let buscar = sexo.find(el=>{
-         return el.checked==true
+        return el.checked==true
     })
     if(buscar==undefined){
-        erro()
+        erro(labelsexo)
+        msgAlert("Marque o campo sexo!", 'alert')
+    }
+    // buscar idade
+    if(diffData() < 13){
+        nascimmento.classList.add("vazio")
+        msgAlert("Idade deve ser maior que 12 anos!", "alert")
+        verify1=false
     }
     // buscar campo vazio
     inputs.map(el=>{
         if(el.value==""){
-            el.classList.add("vazio")
-            verify1 = false; 
+            erro(el) 
+            msgAlert("Campo(s) não preenchido(s)!", 'alert')
         }
     })
-    // senhas diferentes ou menores que 8 digitos
-    if(senha.value!==repetirSenha.value || senha.value.length < 8){
-        repetirSenha.classList.add("vazio")
-        senha.value=''
-        repetirSenha.value=''
-        verify1 = false;
-    }
 } // Fim da validação
 
 // Revover expressão de erro do elemento em foco
@@ -52,6 +62,7 @@ sexo.map(el=>{
     })
 })
 
+// Limpar todos os campos
 function limpar(){
     inputs.map(el=>{
         el.value=''
@@ -74,8 +85,27 @@ formulario.addEventListener("submit", (evt)=>{
     // controlar o envio do formulario
     if(verify1==true){
         usuarioController.adicionar(evt);
+        msgAlert("Cadastro efetuado com sucesso!", "sucess")
         limpar()
     }
     verify1 = true
 })
 
+// Calcular diferença de data
+function diffData(){
+    const b_day = new Date(nascimmento.value)
+    let date = new Date()
+    let yearOld = b_day.getFullYear()
+    let currentYear = date.getFullYear()
+    let diff = currentYear - yearOld
+    return diff
+}
+
+function msgAlert(msg, tipo){
+    messenger.textContent=msg
+    messenger.classList.add(tipo)
+    setTimeout(el=>{
+        messenger.textContent=''
+        messenger.classList.remove(tipo)
+    }, 3000)
+}
