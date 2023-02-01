0a057b9d222c0107130d3b7110d5393a4a2bb02a
RAIMONES <raimonesrg3@gmail.com>
2023-02-01 18:09:23 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,41 +1,54 @@
-import { Show }       from "./poo/view/Show.js";
 import { Controller } from "./poo/controller/Controller.js";
 
-const show = new Show()
 const controller = new Controller()
 
-show.showInit()
-controller.createRepo()
+controller.init()
 
 const submit_register = document.querySelector('.submit-register')
 const inputs          = document.querySelectorAll('input:not([type=radio])')
 const radios          = document.querySelectorAll('input[type=radio]')
 const sexLabel        = document.querySelector('.labelsexo')
 const submit_login    = document.querySelector('#submit-login')
-const toRegister      = document.querySelectorAll('.for-register')
-const toLogin         = document.querySelector('.for-login')
+const toInit          = document.querySelector('.to-init')
+const toRegister      = document.querySelectorAll('.to-register')
+const exit            = document.querySelector('.exit')
+const remove          = document.querySelector('.submit-clear')
+const menuBtn         = document.querySelector('.full-sandwich')
+const s2              = document.querySelector('.s2')
+const s3              = document.querySelector('.s3')
+const menu            = document.querySelector('.nav')
 
+menuBtn.addEventListener('click', ()=>{
+  s2.classList.toggle('open-s2')
+  s3.classList.toggle('open-s3')
+  menu.classList.toggle('disable')
+})
 
 submit_login.addEventListener('click', evt=>{
-
   controller.login(evt)
-
 })
 
 toRegister.forEach(el=>{
   el.addEventListener('click',()=>{
-    show.showRegister()
+    controller.toRegister()
   })
 })
 
-toLogin.addEventListener('click', ()=>{
-  show.showInit()
+toInit.addEventListener('click',()=>{
+  controller.init()
 })
 
 submit_register.addEventListener('click', evt=>{
-  
   controller.register(evt)
+})
 
+exit.addEventListener('click', ()=>{
+  controller.exit()
+})
+
+remove.addEventListener('click', evt=>{
+  evt.preventDefault()
+  controller.deleteAccount()
 })
 
 inputs.forEach(input=>{
